17616f71ddd56648210bf059a8a8f45268a223da
abdulkerimcan <abdulkerimcann@outlook.com>
2023-11-24 17:19:25 +0300

    day1 done

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -1,6 +1,47 @@
 import UIKit
 
+
+// Variables
 var greeting = "Hello, playground"
 
 greeting = "Hello"
 
+var age = 24
+var population = 8_000_000_000
+
+//Multi-Line String
+var str1 = """
+This goes 
+over multiple
+lines
+"""
+
+// multi-line strings to format your code neatly, and you don’t want those line breaks to actually be in your string, end each line with a \, like this:
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+
+
+var pi = 3.141
+
+var awesome = false
+
+//  String interpolation
+
+var score = 100
+
+var str = "Your score is \(score)"
+
+
+// Constants
+
+let taylor = "Swift"
+
+// Type annotations
+
+let album: String = "Reputation"
